878cc2133f5c0dc55b2c58bc198d5b0870a12bda
Byronlee <lbyronlee@gmail.com>
2013-05-28 21:14:46 +0000

    密码显示与隐藏

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -3,3 +3,12 @@ jQuery(function(){
     document.getElementById('time').innerHTML=new Date().toLocaleString()+' 星期'+'日一二三四五六'.charAt(new Date().getDay());
     setInterval("document.getElementById('time').innerHTML=new Date().toLocaleString()+' 星期'+'日一二三四五六'.charAt(new Date().getDay());",1000);
 })
+
+function toggle_password($o,type,pass){
+    if(type=="show"){
+	$o.html('<div class="input-append"><input class="input-medium" disabled="disabled" id="appendedPrependedInput" type="text" value="'+pass+'"><button class="btn toggle" onclick="toggle_password($(this).parents("td"),"hide",'+pass+')" type="button"><i class="icon-eye-open"></i></button></div>')
+    }else{
+	$o.html('<div class="input-append"><input class="input-medium" disabled="disabled" id="appendedPrependedInput" type="password" value="'+pass+'"><button class="btn toggle" onclick="toggle_password($(this).parents("td"),"show",'+pass+')" type="button"><i class="icon-eye-open"></i></button></div>')
+    }
+}
+
--- app/models/member.rb
+++ app/models/member.rb
@@ -3,14 +3,14 @@ class Member
 
   field :website,  type:String
   field :username, type:String
-  field :portkey , type:String
+  field :password, type:String
   field :email,    type:String 
   field :url  ,    type:String
 
-  validates_presence_of :username ,:portkey, :room_id
+  validates_presence_of :username ,:password, :room_id
   validates_length_of :website,  maximum: 10
   validates_length_of :username, maximum: 20
-  validates_length_of :portkey,  maximum: 20
+  validates_length_of :password,  maximum: 20
   validates_length_of :email ,   maximum: 30
   validates_length_of :url,      maximum: 50
 
