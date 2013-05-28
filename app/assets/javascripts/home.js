28800aebc35a69b3be066337ececed4a7678ec82
Byronlee <lbyronlee@gmail.com>
2013-05-28 22:20:01 +0000

    设置默认没有内容的页面

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -4,11 +4,11 @@ jQuery(function(){
     setInterval("document.getElementById('time').innerHTML=new Date().toLocaleString()+' 星期'+'日一二三四五六'.charAt(new Date().getDay());",1000);
 })
 
-function toggle_password($o,type,pass){
-    if(type=="show"){
-	$o.html('<div class="input-append"><input class="input-medium" disabled="disabled" id="appendedPrependedInput" type="text" value="'+pass+'"><button class="btn toggle" onclick="toggle_password($(this).parents("td"),"hide",'+pass+')" type="button"><i class="icon-eye-open"></i></button></div>')
+function toggle_password($o,pass){
+   if($o.find("span[class=pass]").html()=="××××××××××××"){
+	$o.find("span[class=pass]").html(pass)
     }else{
-	$o.html('<div class="input-append"><input class="input-medium" disabled="disabled" id="appendedPrependedInput" type="password" value="'+pass+'"><button class="btn toggle" onclick="toggle_password($(this).parents("td"),"show",'+pass+')" type="button"><i class="icon-eye-open"></i></button></div>')
+	$o.find("span[class=pass]").html("××××××××××××")
     }
 }
 
