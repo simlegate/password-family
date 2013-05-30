be38070c25d4b30b4c30dcbf8c497d9a3edd8bb7
simlegate <simlegate@163.com>
2013-05-30 19:03:39 +0800

    为add room添加验证

--- app/widgets/rooms_widget.rb
+++ app/widgets/rooms_widget.rb
@@ -10,8 +10,12 @@ class RoomsWidget < Apotomo::Widget
   end
 
  def create(evt)
-    @room = Room.create! evt[:room]
-    update :state => :display
+    @room = Room.new(evt[:room])
+    if @room.save
+      update :state => :display
+    else
+      update "#room_validate_#{evt[:id]}",:text => @room.errors.first[1]
+    end
  end
 
  def form arg
@@ -32,5 +36,4 @@ class RoomsWidget < Apotomo::Widget
    room = Room.find(evt[:id])
    update :state => :display if room.destroy 
  end
-
 end
--- spec/factories/member_factory.rb
+++ spec/factories/member_factory.rb
@@ -1,10 +1,10 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :parlour_member ,class: Member do
+  factory :member ,class: Member do
     username 'byronlee'
     email    'lbyronlee@gmail.com'
-    portkey 'lijianghua'
+    cryptogram 'lijianghua'
     website  'baidu'	
     url	'http://www.baidu.com'
   end
